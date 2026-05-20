1999a529b3ca05e475f508a2172c4c4cace013fa
Seb James <sebj_coder3@pm.me>
2026-05-20 17:26:42 +0100

    Specify json and run as movie

--- ant03/ant03.sh
+++ ant03/ant03.sh
@@ -3,4 +3,5 @@
 # Ant03R01 for Fig. 3C
 
 ./build/antpov -f data/seville/ground_and_veg_inner_circular.gltf \
-               -c data/seville/Ant03R01-warped.csv.world.csv -x
+               -c data/seville/Ant03R01-warped.csv.world.csv \
+               -j Ant03R01-warped.csv.world.topdown.json -x -m
