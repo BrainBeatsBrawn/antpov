76f2e475517c300638f90da785c0f36ebbcba439
Seb James <sebj_coder3@pm.me>
2026-05-21 16:57:31 +0100

    Updates to jsons for film direction

--- ant03/ant03.sh
+++ ant03/ant03.sh
@@ -4,4 +4,4 @@
 
 ./build/antpov -f data/seville/ground_and_veg_inner_circular.gltf \
                -c data/seville/Ant03R01-warped.csv.world.csv \
-               -j Ant03R01-warped.csv.world.topdown.json -x -m
+               -j data/seville/Ant03R01-warped.csv.world.json -x -m
