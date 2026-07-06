6d02475ac02c34d1ba2c232f4344de81caa92004
Seb James <sebj_coder3@pm.me>
2026-07-06 08:40:01 +0100

    Renaming

---
+++ scripts/ant03/various_cam_angles_movie.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Ant03R01 for Fig. 3C fancy video
+
+./build/antpov -f data/seville/ground_and_veg_inner_circular.gltf \
+               -c data/seville/Ant03R01-warped.csv.world.csv \
+               -j data/seville/Ant03R01-warped.csv.world.json -x -m
