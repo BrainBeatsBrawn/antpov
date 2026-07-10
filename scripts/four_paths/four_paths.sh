cc9f2b00c63f1056f70bb73f47721647ef327845
Seb James <sebj_coder3@pm.me>
2026-07-10 13:29:02 +0100

    Corrects this script to choose the right 4 ant paths

--- scripts/four_paths/four_paths.sh
+++ scripts/four_paths/four_paths.sh
@@ -23,4 +23,4 @@ fi
 # Once all the paths are complete, move the view around and save screenshots with Ctrl-S
 
 ./build/antpov -f data/seville/ground_and_veg_inner_circular.gltf \
-               -c data/seville/Ant06R07-warped.csv.world.csv,data/seville/Ant03R05-warped.csv.world.csv,data/seville/Ant11R09-warped.csv.world.csv,data/seville/Ant12R02-warped.csv.world.csv -x
+               -c data/seville/Ant06R05-warped.csv.world.csv,data/seville/Ant03R05-warped.csv.world.csv,data/seville/Ant11R06-warped.csv.world.csv,data/seville/Ant12R02-warped.csv.world.csv -x
